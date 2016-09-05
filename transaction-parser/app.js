f2c5396dfc43474a3f09d6cf47f9ced11aac8c60
dan <dgolant@gmail.com>
2016-09-05 00:02:56 -0400

    got sheet loading working

--- transaction-parser/app.js
+++ transaction-parser/app.js
@@ -3,18 +3,24 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var GoogleSpreadsheet = require('google-spreadsheet');
 var async = require('async');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+
+//my modules
 var cronJob = require('cron').CronJob;
 var moment = require('moment');
+var bodyParser = require('body-parser');
+var google = require('googleapis');
+var googleSheets = require('google-spreadsheet');
+
+//auth files
 var tokens = require('./private/tokens.json');
+const keys = require('./private/transactionParserServiceAccountKeys.json');
 
+// Express default stuff
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -36,74 +42,6 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
-
-//main functions
-var doc1 = new GoogleSpreadsheet(JSON.stringify(tokens.googleSheet1Token));
-var sheet1;
-
-async.series([
-    function setAuth(step) {
-        // see notes below for authentication instructions!
-        var creds = require('./private/transactionParserAppEngineKey.json');
-        doc1.useServiceAccountAuth(creds, step);
-    },
-    function startCron(step) {
-        console.log('HIT THE CRON');
-        var stationInformationJob = new cronJob("*/20 * * * * *", function() {
-                console.log('//////////////////////////////');
-                var currentTime = moment().format('YYYY MMMM DD HH:mm:ss');
-                console.log('data extract job STARTED at ' + currentTime);
-                async.series([
-                    function getInfoAndWorksheets(step) {
-                        doc1.getInfo(function(err, info) {
-                            console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
-                            sheet = info.worksheets[0];
-                            console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
-                            cb();
-                        });
-                    }
-                ]);
-
-
-
-
-
-
-
-
-
-
-            },
-            function() {
-                /* this function  is run  when the is stopped*/
-            },
-            //start immediately
-            true,
-            //timezone
-            'America/New_York');
-    }
-]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // error handlers
 
@@ -128,5 +66,61 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+//End Express Default Stuff
+var doc1 = new googleSheets(tokens.googleSheet1Token);
+var sheet;
+
+
+var daily = new cronJob("*/20 * * * * *", function() {
+        console.log('//////////////////////////////');
+        var currentTime = moment().format('YYYY MMMM DD HH:mm:ss');
+        console.log('Google API Auth Started:' + currentTime);
+        async.series([
+            function setAuth(step) {
+                var creds_json = {
+                    client_email: keys.client_email,
+                    private_key: keys.private_key
+                }
+
+                doc1.useServiceAccountAuth(creds_json, step);
+                console.log('hit');
+            },
+            function getInfoAndWorksheets(step) {
+                doc1.getInfo(function(err, info) {
+                    console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+                    sheet = info.worksheets[0];
+                    console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
+                    var lastParseTime = moment().format('YYYY MMMM DD HH:mm:ss');
+                    step();
+                });
+            }
+        ]);
+    },
+    function workingWithRows(step) {
+        // google provides some query options
+        sheet.getRows({
+            offset: 1,
+            limit: 20,
+            orderby: 'col2'
+        }, function(err, rows) {
+            console.log('Read ' + rows.length + ' rows');
+
+            // the row is an object with keys set by the column headers
+            // rows[0].colname = 'new val';
+            // rows[0].save(); // this is async
+
+            // // deleting a row 
+            // rows[0].del(); // this is async
+
+            step();
+        });
+    },
+    function() {
+        /* this function  is run  when the is stopped*/
+    },
+    //start immediately
+    true,
+    //timezone
+    'America/New_York');
 
 module.exports = app;
