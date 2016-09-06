06fcafd98d179afa6737d01dbe0c0fdb0dd3725f
dan <dgolant@gmail.com>
2016-09-05 23:14:22 -0400

    parsing out transactions and locations

--- transaction-parser/app.js
+++ transaction-parser/app.js
@@ -14,6 +14,7 @@ var moment = require('moment');
 var bodyParser = require('body-parser');
 var google = require('googleapis');
 var googleSheets = require('google-spreadsheet');
+var VerEx = require('verbal-expressions');
 
 //auth files
 var tokens = require('./private/tokens.json');
@@ -83,37 +84,43 @@ var daily = new cronJob("*/20 * * * * *", function() {
                 }
 
                 doc1.useServiceAccountAuth(creds_json, step);
-                console.log('hit');
             },
             function getInfoAndWorksheets(step) {
                 doc1.getInfo(function(err, info) {
                     console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
                     sheet = info.worksheets[0];
                     console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
-                    var lastParseTime = moment().format('YYYY MMMM DD HH:mm:ss');
+                    //var lastParseTime = moment().format('YYYY MMMM DD HH:mm:ss');
+                    step();
+                });
+            },
+            function workingWithRows(step) {
+                // google provides some query options
+                sheet.getRows({
+                    offset: 1,
+                    limit: 2000,
+                    orderby: 'col2'
+                }, function(err, rows) {
+                    console.log('Read ' + rows.length + ' rows');
+
+                    for (var i = 0; i < rows.length; i++) {
+                        if (rows[i].transaction.includes('($USD)')) {
+                            var transaction = rows[i].transaction;
+                            //the first index of matchStringArray is the string you need, the rest is superfluous
+                            var matchStringArray = transaction.match(/charge of.*?has/);
+                            var etlStringArray = matchStringArray[0].split(/at/);
+                            var TransactionAmt = etlStringArray[0].replace('charge of ($USD) ', '$');
+                            var TransactionLocation = etlStringArray[1].replace(' has', '');
+                            console.log('-----Transaction '+i+' -----');
+                            console.log('Amount:' +TransactionAmt);
+                            console.log('Location:' +TransactionLocation);
+                        }
+                    }
+
                     step();
                 });
             }
         ]);
-    },
-    function workingWithRows(step) {
-        // google provides some query options
-        sheet.getRows({
-            offset: 1,
-            limit: 20,
-            orderby: 'col2'
-        }, function(err, rows) {
-            console.log('Read ' + rows.length + ' rows');
-
-            // the row is an object with keys set by the column headers
-            // rows[0].colname = 'new val';
-            // rows[0].save(); // this is async
-
-            // // deleting a row 
-            // rows[0].del(); // this is async
-
-            step();
-        });
     },
     function() {
         /* this function  is run  when the is stopped*/
@@ -121,6 +128,7 @@ var daily = new cronJob("*/20 * * * * *", function() {
     //start immediately
     true,
     //timezone
-    'America/New_York');
+    // 'America/New_York'
+);
 
 module.exports = app;
