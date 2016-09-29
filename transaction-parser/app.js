3a6099e5e8c407b6a12bc3cf8f53090f2b946cf9
dan <dgolant@gmail.com>
2016-09-29 07:54:20 -0400

    working pull

--- transaction-parser/app.js
+++ transaction-parser/app.js
@@ -69,66 +69,83 @@ app.use(function(err, req, res, next) {
 });
 //End Express Default Stuff
 var doc1 = new googleSheets(tokens.googleSheet1Token);
-var sheet;
+var sheet1;
+var doc2 = new googleSheets(tokens.googleSheet2Token);
+var sheet2;
 
 
-var daily = new cronJob("*/20 * * * * *", function() {
-        console.log('//////////////////////////////');
-        var currentTime = moment().format('YYYY MMMM DD HH:mm:ss');
-        console.log('Google API Auth Started:' + currentTime);
-        async.series([
-            function setAuth(step) {
-                var creds_json = {
-                    client_email: keys.client_email,
-                    private_key: keys.private_key
+
+
+var currentTime = moment().format('YYYY MMMM DD HH:mm:ss');
+console.log('Google API Auth Started:' + currentTime);
+async.series([
+    function setAuthOne(step) {
+        var creds_json = {
+            client_email: keys.client_email,
+            private_key: keys.private_key
+        }
+
+        doc1.useServiceAccountAuth(creds_json, step);
+    },
+    function setAuthTwo(step) {
+        var creds_json = {
+            client_email: keys.client_email,
+            private_key: keys.private_key
+        }
+
+        doc2.useServiceAccountAuth(creds_json, step);
+    },
+    function getInfoAndWorksheets(step) {
+        doc1.getInfo(function(err, info) {
+            console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+            sheet1 = info.worksheets[0];
+            console.log('sheet 1: ' + sheet1.title + ' ' + sheet1.rowCount + 'x' + sheet1.colCount);
+            //var lastParseTime = moment().format('YYYY MMMM DD HH:mm:ss');
+            // step();
+        });
+
+        doc2.getInfo(function(err, info) {
+            console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+            sheet2 = info.worksheets[0];
+            console.log('sheet 2: ' + sheet2.title + ' ' + sheet2.rowCount + 'x' + sheet2.colCount);
+            //var lastParseTime = moment().format('YYYY MMMM DD HH:mm:ss');
+            step();
+        });
+    },
+    function workingWithRows(step) {
+        // google provides some query options
+        sheet1.getRows({
+            offset: 1,
+            limit: 2000,
+            orderby: 'col2'
+        }, function(err, rows) {
+            console.log('Read ' + rows.length + ' rows');
+
+            for (var i = 0; i < rows.length; i++) {
+                if (rows[i].transaction.includes('($USD)') && !(rows[i].transaction.includes('<!DOCTYPE HTML PUBLIC'))) {
+                    // here we check that it doesnt include html tags because sometimes chase sends alerts that
+                    // somehow get into
+                    // the original spreadsheet that are not actually spending alerts, but like, payment alerts or other things.
+                    // all those extraneous alerts seem to contain hella HTML as opposed to the spending alerts, so
+                    // they seem like a reasonable indicator
+                    // by which to omit cells.  YMMV (this comment is terrible style I know)
+
+                    var transaction = rows[i].transaction;
+                    //the first index of matchStringArray is the string you need, the rest is superfluous
+                    var matchStringArray = transaction.match(/charge of.*?has/);
+                    var etlStringArray = matchStringArray[0].split(/at/);
+                    var TransactionAmt = etlStringArray[0].replace('charge of ($USD) ', '$');
+                    var TransactionLocation = etlStringArray[1].replace(' has', '');
+
+                    console.log('-----Transaction ' + i + '-----');
+                    console.log('Amount:' + TransactionAmt);
+                    console.log('Location:' + TransactionLocation);
                 }
-
-                doc1.useServiceAccountAuth(creds_json, step);
-            },
-            function getInfoAndWorksheets(step) {
-                doc1.getInfo(function(err, info) {
-                    console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
-                    sheet = info.worksheets[0];
-                    console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
-                    //var lastParseTime = moment().format('YYYY MMMM DD HH:mm:ss');
-                    step();
-                });
-            },
-            function workingWithRows(step) {
-                // google provides some query options
-                sheet.getRows({
-                    offset: 1,
-                    limit: 2000,
-                    orderby: 'col2'
-                }, function(err, rows) {
-                    console.log('Read ' + rows.length + ' rows');
-
-                    for (var i = 0; i < rows.length; i++) {
-                        if (rows[i].transaction.includes('($USD)')) {
-                            var transaction = rows[i].transaction;
-                            //the first index of matchStringArray is the string you need, the rest is superfluous
-                            var matchStringArray = transaction.match(/charge of.*?has/);
-                            var etlStringArray = matchStringArray[0].split(/at/);
-                            var TransactionAmt = etlStringArray[0].replace('charge of ($USD) ', '$');
-                            var TransactionLocation = etlStringArray[1].replace(' has', '');
-                            console.log('-----Transaction '+i+' -----');
-                            console.log('Amount:' +TransactionAmt);
-                            console.log('Location:' +TransactionLocation);
-                        }
-                    }
-
-                    step();
-                });
             }
-        ]);
-    },
-    function() {
-        /* this function  is run  when the is stopped*/
-    },
-    //start immediately
-    true,
-    //timezone
-    // 'America/New_York'
-);
+
+            step();
+        });
+    }
+]);
 
 module.exports = app;
